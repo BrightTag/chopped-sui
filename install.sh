5e39262f3fa725a4ab66e696d681c280ff0c74f5
Art Lawry <alawry@signal.co>
2014-12-17 08:51:16 -0600

    conditional install script and a demo markup fix

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";brew install node;npm install;npm install gulp -g;sudo gem install bundler;bundler install;gulp;gulp start:demo;
+#
+if ! type brew >/dev/null 2>&1; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  brew update
+fi
+if ! type npm >/dev/null 2>&1; then
+  brew install node
+fi
+npm install
+if ! type gulp >/dev/null 2>&1; then
+  npm install gulp -g
+fi
+if ! type bundler >/dev/null 2>&1; then
+  sudo gem install bundler
+fi
+bundler install
+gulp
--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npm install;gulp;gulp start:demo;
+npm install
+gulp
